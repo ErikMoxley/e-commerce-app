ef31342ca871ecfb3756e8b7366d341c006278fd
ErickMoxley <exilerzero@yahoo.com>
2020-03-06 03:40:28 -0500

    add signin page form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     </Router>
