04fc537a77cedd4ca84f6cb30cf420ea0639fa48
ErickMoxley <exilerzero@yahoo.com>
2020-03-06 02:29:08 -0500

    added shop page, items and previews

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     </Router>
