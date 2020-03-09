03d8f389821a1897d14797e311ab25ff2dc767e2
ErickMoxley <exilerzero@yahoo.com>
2020-03-09 02:29:32 -0400

    update

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
             type="email"
             value={this.state.email}
             handleChange={this.handleChange}
-            label="email"
+            label="Email"
             required
           />
           <FormInput
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
             type="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label="Password"
             required
           />
           <div className="buttons">
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+//where I'd enter Firebase APIkey.
 const config = {
   apiKey: "AIzaSyD3KV7zoMnivXdzHBN4-63t1To7W6u418w",
   authDomain: "e-commerce-app-b6133.firebaseapp.com",
@@ -11,6 +12,30 @@ const config = {
   messagingSenderId: "618869871245",
   appId: "1:618869871245:web:2ccc875ccfe937662b88c4",
   measurementId: "G-PMGP1V6DS9"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
