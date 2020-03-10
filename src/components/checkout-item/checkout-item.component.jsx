1bd109805700d3b8e2498c330c687a5acab48366
ErickMoxley <exilerzero@yahoo.com>
2020-03-10 08:15:36 -0400

    npm installed react-persist to allow me to utilize local storage as default. Persistor used to make this happen.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -17,11 +17,15 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
       <span className="name">{name}</span>
       <span className="quantity">
         <div className="arrow" onClick={() => removeItem(cartItem)}>
-          <span>&#10134;</span>
+          <span aria-label="minus" role="img">
+            &#x2796;
+          </span>
         </div>
         <span className="value">{quantity}</span>
         <div className="arrow" onClick={() => addItem(cartItem)}>
-          <span>&#10133;</span>
+          <span aria-label="plus" role="img">
+            &#x2795;
+          </span>
         </div>
       </span>
       <span className="price">${price}</span>
